32f8c13886276e05dfae0c0bbab7e18eab48082d
JawandS <Jawandsingh@gmail.com>
2022-11-27 19:09:51 +0000

    change update method for model

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo python3 model.py "$1" & sudo bpftrace -e 'tracepoint:sched:sched_switch { printf("%s %lu %d %lu\n", comm, pid, cpu, nsecs); }' >> Logs/log_"$1".txt
-sudo ./update.sh "$1"
+sudo ./update.sh "add and process $1"
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 find . -size +99M | cat >> .gitignore
 git commit -m "update .gitignore"
 git add .
-git commit -m $1
+git commit -m "$1"
 git push
