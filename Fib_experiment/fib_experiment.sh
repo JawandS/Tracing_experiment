fc131d7a4a4aeebe07ce1953cf13a2e843f4c9ed
JawandS <Jawandsingh@gmail.com>
2022-11-27 21:35:03 +0000

    update

--- Fib_experiment/fib_experiment.sh
+++ Fib_experiment/fib_experiment.sh
@@ -4,4 +4,9 @@ do
   python3 job.py $counter &
 done
 sudo bpftrace -e 'tracepoint:sched:sched_switch { printf("%s %lu %d %lu\n", comm, pid, cpu, nsecs); }' >> Logs/log_"$1".txt &
-sudo python3 auto_kill.py 9 && sudo python3 processing.py "$1" && .././update.sh
+sudo python3 auto_kill.py 9 && sudo python3 processing.py "$1"
+find . -size +99M | cat >> ../.gitignore
+git commit -m "update .gitignore"
+git add .
+git commit -m "add and process $1"
+git push
--- Fib_experiment/update.sh
+++
@@ -1,5 +0,0 @@
-find . -size +99M | cat >> ../.gitignore
-git commit -m "update .gitignore"
-git add .
-git commit -m "$1"
-git push
