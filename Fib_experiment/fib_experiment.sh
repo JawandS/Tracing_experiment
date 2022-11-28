ccd7879d170c6bfc503d768c266144c0d1565e97
Jawand <jawandsingh@gmail.com>
2022-11-28 14:09:27 -0500

    minor edit

--- Fib_experiment/fib_experiment.sh
+++ Fib_experiment/fib_experiment.sh
@@ -4,7 +4,7 @@ do
   python3 job.py $counter &
 done
 sudo bpftrace -e 'tracepoint:sched:sched_switch { printf("%s %lu %d %lu\n", comm, pid, cpu, nsecs); }' >> Logs/log_"$1".txt &
-sudo python3 auto_kill.py 9 && sudo python3 processing.py "$1"
+sudo python3 auto_kill.py && sudo python3 processing.py "$1"
 find . -size +99M | cat >> ../.gitignore
 git commit -m "update .gitignore"
 git add .
