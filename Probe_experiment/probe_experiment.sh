56df1ebd7cc77b2329b1815ff99f32b4ba20d1de
Jawand <jawandsingh@gmail.com>
2022-11-30 09:48:51 -0500

    process probe experiment

--- Probe_experiment/probe_experiment.sh
+++ Probe_experiment/probe_experiment.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 for counter in {1..20}
 do
-    sudo python3 model.py -1 quiet >> Results/run"$1".txt & sudo bpftrace two_probes.bt >> /dev/null
-    sudo python3 model.py -1 quiet >> Results/run"$1".txt
+    sudo python3 model.py -1 quiet >> Logs/run"$1".txt & sudo bpftrace two_probes.bt >> /dev/null
+    sudo python3 model.py -1 quiet >> Logs/run"$1".txt
     echo $counter
 done
+sudo python3 processing.py "$1"
 find . -size +99M | cat >> ./.gitignore
 git commit -m "update .gitignore"
 git add .
-git commit -m "$1"
+git commit -m "add and process probe experiment $1"
 git push
---
+++ Probe_experiment/processing.py
@@ -0,0 +1,46 @@
+def read_file(file):
+    # add file lines to list
+    lines = []
+    with open(file, 'r') as f:
+        for line in f:
+            if ln := line.strip():
+                lines.append(round(float(ln), 3))
+    return lines
+
+
+def get_data(lines):
+    # split into tracing and not tracing runs
+    tracing_runs = []
+    model_runs = []
+    for counter, line in enumerate(lines):
+        if counter % 2 == 0:
+            tracing_runs.append(line)
+        else:
+            model_runs.append(line)
+    # process average min and max
+    tracing_info = (round(sum(tracing_runs) / len(tracing_runs), 5), min(tracing_runs), max(tracing_runs))
+    model_info = (round(sum(model_runs) / len(model_runs), 5), min(model_runs), max(model_runs))
+    difference_info = (round(tracing_info[0] - model_info[0], 5), round(tracing_info[1] - model_info[1], 5),
+                       round(tracing_info[2] - model_info[2], 5))
+    return tracing_info, model_info, difference_info
+
+
+if __name__ == "__main__":
+    # get the run number
+    import sys
+
+    args = sys.argv
+    if len(args) > 1:
+        run = args[1]
+    else:
+        run = '1'
+    # read file
+    lines = read_file("Logs/run" + run + ".txt")
+    # process data
+    tracing_info, model_info, difference_info = get_data(lines)
+    # write results to file
+    with open("Results/result" + run + ".txt", 'w') as f:
+        f.write("average, min, max\n")
+        f.write("Tracing: " + str(tracing_info) + "\n")
+        f.write("Model: " + str(model_info) + "\n")
+        f.write("Model: " + str(difference_info) + "\n")
