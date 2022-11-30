5d832cf11851b5f739ba64dae00d192cc6bd63a3
Jawand <jawandsingh@gmail.com>
2022-11-30 09:34:04 -0500

    update script

--- Probe_experiment/probe_experiment.sh
+++ Probe_experiment/probe_experiment.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 for counter in {1..20}
 do
-    sudo python3 model.py -1 quiet >> Results/run"$1".txt & sudo bpftrace two_probes.bt >> junk.txt
+    sudo python3 model.py -1 quiet >> Results/run"$1".txt & sudo bpftrace two_probes.bt >> /dev/null
     sudo python3 model.py -1 quiet >> Results/run"$1".txt
     echo $counter
 done
-find . -size +99M | cat >> ../.gitignore
+find . -size +99M | cat >> ./.gitignore
 git commit -m "update .gitignore"
 git add .
 git commit -m "$1"
