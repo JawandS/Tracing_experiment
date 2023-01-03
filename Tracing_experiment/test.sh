2933ec960a8cb4aa9c30ffc8d826ab67e80164a7
Jawand <jawandsingh@gmail.com>
2023-01-03 17:58:25 -0500

    update test

--- Tracing_experiment/test.sh
+++ Tracing_experiment/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 counter=0
-sudo when-changed tracking.txt 'echo $counter && counter=$((counter+1)) && echo "second print " && echo $counter';
+counter=((counter+1))
+while inotifywait -e close_write tracking.txt; do "echo $counter; echo $counter"; done
+#sudo when-changed tracking.txt 'echo $counter && counter=$((counter+1)) && echo "second print " && echo $counter';
 echo $counter
