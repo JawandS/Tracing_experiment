2ed50cf16c54b970ef385159c08251f0adcc7b17
JawandS <JawandSingh@gmail.com>
2023-01-03 23:05:00 +0000

    fixed test

--- Tracing_experiment/test.sh
+++ Tracing_experiment/test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 counter=0
-counter=((counter+1))
-while inotifywait -e close_write tracking.txt; do "echo $counter; echo $counter"; done
+while inotifywait -e close_write tracking.txt; do ((counter=counter+1)) && if [ $counter -ge 5 ]; then kill $(ps aux | grep '[b]pftrace' | awk '{print $2}'); fi; done
 #sudo when-changed tracking.txt 'echo $counter && counter=$((counter+1)) && echo "second print " && echo $counter';
 echo $counter
