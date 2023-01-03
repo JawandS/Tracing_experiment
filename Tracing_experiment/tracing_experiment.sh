6ff7e2b44afeb64dd8a2d10fbe424fa59e5d7d7d
Jawand <jawandsingh@gmail.com>
2023-01-02 20:32:21 -0500

    fix ts issue

--- Tracing_experiment/tracing_experiment.sh
+++ Tracing_experiment/tracing_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for run_num in {1..20}
+for run_num in {1..3} # number of total iterations
 do
-  echo time +%N >> Logs/four_fib_"$1".txt
+  echo date +"%T.%N" >> Logs/four_fib_"$1".txt # starting time for tracing set
   # first job - with tracing
   sudo bpftrace context_switch_probe.bt >> raw.txt & sudo python3 auto_kill.py &
   for a_var in {1...20}
@@ -9,13 +9,13 @@ do
     python3 job.py a_var 25 &
     sleep 0.015
   done
-  echo time +%N >> Logs/four_fib_"$1".txt
+  echo date +"%T.%N" >> Logs/four_fib_"$1".txt # end tracing, start non tracing
   # second job - without tracing
   for b_var in {1...20}
   do
     python3 job.py b_var 25 &
   done
-  echo time +%N >> Logs/four_fib_"$1".txt
+  echo date +"%T.%N" >> Logs/four_fib_"$1".txt # end non tracing
 done
 sudo python3 processing.py "$1"
 find . -size +98M | cat >> .gitignore
