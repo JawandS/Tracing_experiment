ac8ac026297b8ac983fd0ee2a952c6287168d691
JawandS <JawandSingh@gmail.com>
2023-01-03 16:23:23 +0000

    modify script

--- Tracing_experiment/tracing_experiment.sh
+++ Tracing_experiment/tracing_experiment.sh
@@ -2,15 +2,16 @@
 for run_num in {1..3} # number of total iterations
 do
   echo date +"%T.%N" >> Logs/log_"$1".txt # starting time for tracing set
-  # first job - with tracing
-  sudo bpftrace context_switch_probe.bt >> raw.txt & sudo python3 auto_kill.py &
+  counter=0
+  # first set - with tracing
+  sudo bpftrace context_switch_probe.bt >> raw.txt & sudo when-changed tracking.txt counter=$counter+1; car $counter &
   for a_var in {1...20}
   do
-    python3 job.py $a_var 25 &
+    python3 job.py $a_var 25 >> tracking.txt &
     sleep 0.015
   done
   echo date +"%T.%N" >> Logs/log_"$1".txt # end tracing, start non tracing
-  # second job - without tracing
+  # second set - without tracing
   for b_var in {1...20}
   do
     python3 job.py $b_var 25 &
