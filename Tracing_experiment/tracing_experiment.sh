250ad4f8389d6358e1dfc8ea9ef721fa4565c784
Jawand <jawandsingh@gmail.com>
2023-01-04 09:48:30 -0500

    modify tracing experiment for testing

--- Tracing_experiment/tracing_experiment.sh
+++ Tracing_experiment/tracing_experiment.sh
@@ -4,16 +4,16 @@ do
   echo date +"%T.%N" >> Logs/log_"$1".txt # starting time for tracing set
   counter=0
   # first set - with tracing
-  sudo bpftrace context_switch_probe.bt >> raw.txt & while inotifywait -e -m modify tracking.txt; do ((counter=counter+1)) && echo "$counter" && if [ "$counter" -ge 20 ]; then kill "$(ps aux | grep '[b]pftrace' | awk '{print $2}')" && echo "killing jobs"; fi; done &
+  sudo bpftrace context_switch_probe.bt >> raw.txt & while inotifywait -q -q -e modify tracking.txt; do ((counter=counter+1)) && echo "$counter" && if [ "$counter" -ge 20 ]; then kill "$(ps aux | grep '[b]pftrace' | awk '{print $2}')" && echo "killing jobs"; fi; done &
   for a_var in {1...20}
   do
-    python3 job.py "$a_var" 25 >> tracking.txt & sleep 0.015
+    python3 job.py "$a_var" 30 >> tracking.txt & sleep 0.015
   done
   echo date +"%T.%N" >> Logs/log_"$1".txt # end tracing, start non tracing
   # second set - without tracing
 #  for b_var in {1...20}
 #  do
-#    python3 job.py "$b_var" 25 & sleep 0.015
+#    python3 job.py "$b_var" 30 & sleep 0.015
 #  done
 #  echo date +"%T.%N" >> Logs/log_"$1".txt # end non tracing
 done
