eaca863969ca4088d1e7ea36ea09f19467ecacb9
Jawand <jawandsingh@gmail.com>
2023-01-04 18:33:41 -0500

    recreate test

---
+++ Tracing_experiment/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+counter=0
+sudo bpftrace context_switch_probe.bt >> /dev/null &
+while inotifywait -e close_write tracking.txt; do counter=$((counter+1)) && if [ $counter -ge 5 ]; then kill $(ps aux | grep '[b]pftrace' | awk '{print $2}'); fi; done
+#sudo when-changed tracking.txt 'echo $counter && counter=$((counter+1)) && echo "second print " && echo $counter';
+echo $counter
