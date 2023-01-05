e8c16e4ee7dfa5f0934d040cf46d02827dcddec7
Jawand <jawandsingh@gmail.com>
2023-01-05 18:13:17 -0500

    clean experiment

--- Tracing_experiment/experiment.sh
+++ Tracing_experiment/experiment.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+truncate -s 0 file.txt; killall python3; killall bpftrace # clear output file and kill processes
 for _ in {1..10} # number of iterations
 do
   # first set - with tracing
@@ -6,7 +7,7 @@ do
   sudo bpftrace context_switch_probe.bt >> raw.txt & # being tracing
   end=$((SECONDS+10)) # 10 seconds
   while [ $SECONDS -lt $end ]; do # continue for 10 seconds
-      python3 job.py $tracing_counter 27 >> /dev/null &&
+      python3 job.py $tracing_counter 10 27 >> /dev/null &&
       tracing_counter=$((tracing_counter+1)) # run job and increment counter
   done
   # killall python3 && echo "kill python"
@@ -16,9 +17,10 @@ do
   simple_counter=0 # number of fib jobs completed
   end=$((SECONDS+10)) # 10 seconds
   while [ $SECONDS -lt $end ]; do # continue for 10 seconds
-      python3 job.py $simple_counter 27 >> /dev/null &&
+      python3 job.py $simple_counter 10 27 >> /dev/null &&
       simple_counter=$((simple_counter+1)) # run job and increment counter
   done
   # killall python3 && echo "kill python"
   echo $simple_counter >> Logs/log_"$1".txt && echo "standard produced $simple_counter" # output to log
 done
+git add .; git commit -m "add and process overhead experiment $1"; git push # add to git
--- Tracing_experiment/job.py
+++ Tracing_experiment/job.py
@@ -1,4 +1,5 @@
-import time, sys, os, signal
+import time, sys
+from _thread import start_new_thread
 
 
 # fibonacci function using recursion
@@ -8,10 +9,18 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)
 
 
+# multiple fibonacci threads
+def server_workload(T, n):
+    for _ in range(T):
+        start_new_thread(fib, (n,))
+
+
 # Driver Program
 if __name__ == "__main__":
     args = sys.argv[1:]  # job number and depth
     start_time = time.time()
-    n = int(args[1])  # depth of fib job
-    fib(n) # run fib job
-    print(f"job {args[0]}, depth {n}, {round(time.time() - start_time, 3)}s") # output job info
+    name = args[0]  # name of the job
+    T = int(args[1])  # number of threads
+    n = int(args[2])  # depth of fib job
+    server_workload(T, n)  # run fib job
+    # print(f"{name}, {T} threads, depth {n}, {round(time.time() - start_time, 3)}s") # output job info
