71cde37cfbe2d3f1aef39cf25c7906d8fb623c3a
Jawand <jawandsingh@gmail.com>
2023-01-06 08:17:33 -0500

    update experiment

--- Tracing_experiment/processing.py
+++ Tracing_experiment/processing.py
@@ -13,19 +13,24 @@ def read_file(file):
 
 def get_data(lines):
     # split into tracing and not tracing runs
+    two_run = []
     tracing_run = []
     standard_run = []
-    for counter, line in enumerate(lines):
-        if counter % 2 == 0:
-            tracing_run.append(line)
-        else:
-            standard_run.append(line)
+    for counter in range(len(lines), 3):
+        two_run.append(lines[counter])
+        tracing_run.append(lines[counter + 1])
+        standard_run.append(lines[counter + 2])
     # process average min and max
+    two_info = (round(sum(two_run) / len(two_run), 3), min(two_run), max(two_run))
     tracing_info = (round(sum(tracing_run) / len(tracing_run), 3), min(tracing_run), max(tracing_run))
     standard_info = (round(sum(standard_run) / len(standard_run), 3), min(standard_run), max(standard_run))
-    difference_info = [standard_run[i] - tracing_run[i] for i in range(len(tracing_run))]
-    total_difference = round(sum(standard_run) / sum(tracing_run), 3)
-    return tracing_info, standard_info, difference_info, total_difference
+    difference_info = (two_run, tracing_run, standard_run)
+
+    def calc_diff(runA, runB):
+        round(sum(runA) / sum(runB), 3)
+
+    total_difference = [calc_diff(two_run, standard_run), calc_diff(tracing_run, standard_run), calc_diff(tracing_run, two_run)]
+    return two_info, tracing_info, standard_info, difference_info, total_difference
 
 
 if __name__ == "__main__":
@@ -38,12 +43,14 @@ if __name__ == "__main__":
     # read file
     lines = read_file("Logs/log_" + run + ".txt")
     # process data
-    tracing_info, standard_info, diff_runs, total_difference = get_data(lines)
+    two_info, tracing_info, standard_info, diff_runs, total_difference = get_data(lines)
     # write results to file
     with open("Results/result_" + run + ".txt", 'w') as f:
         f.write(f"iterations {args[2]} time {args[3]} threads {args[4]} depth {args[5]}\n")
         f.write("              average, min, max\n")
+        f.write("Two probes  : " + str(tracing_info) + "\n")
         f.write("Tracing runs: " + str(tracing_info) + "\n")
         f.write("Normal runs : " + str(standard_info) + "\n")
-        f.write("Differences: " + str(diff_runs) + "\n")
-        f.write("Total diff: " + str(total_difference) + "\n")
+        f.write("Two tracing standard: " + str(diff_runs) + "\n")
+        f.write("Two vs standard | tracing vs standard | tracing vs two\n")
+        f.write("Total diffs: " + str(total_difference) + "\n")
