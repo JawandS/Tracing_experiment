a8ce1b526cf4ad2f4d3a73278afb2b68752608cc
Jawand <jawandsingh@gmail.com>
2023-01-11 10:09:05 -0500

    update experiment.sh

--- Tracing_experiment/experiment.sh
+++ Tracing_experiment/experiment.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+runNumber=1
 # run experiment
 for _ in {1..20}; do # number of iterations
+  printf "$'\t'---------"$'\t'""
   # clear output file and kill processes
   truncate -s 0 raw.txt
   truncate -s 0 rawTwo.txt
-  killall python3 >>/dev/null
-  killall bpftrace >>/dev/null
+  killall -q python3
+  killall -q bpftrace
   # first set - with two probes tracing
   sleep 1       # wait for 1 second
   two_counter=0 # number of fib jobs completed
@@ -16,20 +18,20 @@ for _ in {1..20}; do # number of iterations
     python3 job.py two_counter 15 27 >>/dev/null &&
       two_counter=$((two_counter + 1)) # run job and increment counter
   done
-  echo $two_counter >>Logs/log_"$1".txt && echo "two probes $two_counter" # output to log
-  killall bpftrace && echo "kill bpftrace"                                # end tracing
+  echo $two_counter >>Logs/log_"$1".txt && echo "Two probes: $two_counter" # output to log
+  killall bpftrace                                                         # end tracing
   # second set - with tracing
   sleep 1           # wait for 1 second
   tracing_counter=0 # number of fib jobs completed
   # shellcheck disable=SC2024
-  sudo bpftrace context_switch_probe.bt >>raw.txt & # being tracing
+  sudo bpftrace context_switch_probe.bt >>raw.txt & # begin tracing
   end=$((SECONDS + 20))                             # 10 seconds
   while [ $SECONDS -lt $end ]; do                   # continue for 10 seconds
     python3 job.py $tracing_counter 15 27 >>/dev/null &&
       tracing_counter=$((tracing_counter + 1)) # run job and increment counter
   done
-  echo $tracing_counter >>Logs/log_"$1".txt && echo "tracing set got through $tracing_counter" # output to log
-  killall bpftrace && echo "kill bpftrace"                                                     # end tracing
+  echo $tracing_counter >>Logs/log_"$1".txt && echo "Tracing: $tracing_counter" # output to log
+  killall bpftrace                                                              # end tracing
   # third set - without tracing
   sleep 1                         # wait for 1 second
   simple_counter=0                # number of fib jobs completed
@@ -39,13 +41,14 @@ for _ in {1..20}; do # number of iterations
       simple_counter=$((simple_counter + 1)) # run job and increment counter
   done
   # killall python3 && echo "kill python"
-  echo $simple_counter >>Logs/log_"$1".txt && echo "standard produced $simple_counter" # output to log
+  echo $simple_counter >>Logs/log_"$1".txt && echo "Standard: $simple_counter" # output to log
   echo wc -l rawTwo.txt >>Logs/log_"$1".txt
   echo wc -l raw.txt >>Logs/log_"$1".txt
+  echo "Run $runNumber Complete" && runNumber=$((runNumber + 1))
 done
 #echo "info: run number, iterations, time, threads, depth" >>Logs/log_"$1".txt
 #echo "      $1          20          30s   15       27" >>Logs/log_"$1".txt
- # add line numbers to info file
+# add line numbers to info file
 python3 processing.py "$1" 20 20s 15 27 # run number, iterations, time, threads, depth
 git add .
 git commit -m "add and process overhead experiment $1"
