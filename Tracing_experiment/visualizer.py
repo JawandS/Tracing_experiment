1f6afeb8ce293009aeaad59c7826061ea505db5c
Jawand <jawandsingh@gmail.com>
2023-01-13 10:15:50 -0500

    fix visualizer.py

--- Tracing_experiment/visualizer.py
+++ Tracing_experiment/visualizer.py
@@ -1,26 +1,34 @@
 import sys
-
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-args = sys.argv
-if len(args) > 1:
-    run_num = args[1]
-else:
-    run_num = "5"
-with open(f"Results/result_{run_num}.txt") as file:
-    #  get data
-    lines = [line.rstrip() for line in file]
-    TIME = 20  # length of each job set
-    twoProbes = [TIME / elem for elem in eval(lines[8])]
-    tracing = [TIME / elem for elem in eval(lines[10])]
-    standard = [TIME / elem for elem in eval(lines[12])]
-    runNums = [i + 1 for i in range(len(twoProbes))]
-    data_preproc = pd.DataFrame(
-        {"Run": runNums, "Two Probes": [((twoProbes[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
-         "Tracing": [((tracing[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
-         "Standard": [((standard[i] / standard[i]) - 1) * 100 for i in range(len(standard))]})
-    # plot data
-    fig = sns.scatterplot(x='Run', y='value', hue='variable', data=pd.melt(data_preproc, ['Run']))
-    fig.set(xlabel='Run Number', ylabel='Percent Decrease from Standard')
-    fig.get_figure().savefig(f"Figures/figure_{run_num}.png")
+def main(run_num):
+    with open(f"Results/result_{run_num}.txt") as file:
+        #  get data
+        lines = [line.rstrip() for line in file]
+        TIME = 20  # length of each job set
+        twoProbes = [elem for elem in eval(lines[8])]
+        tracing = [elem for elem in eval(lines[10])]
+        standard = [elem for elem in eval(lines[12])]
+        runNums = [i + 1 for i in range(len(twoProbes))]
+        data_preproc = pd.DataFrame(
+            {"Run": runNums, "Two Probes": [((twoProbes[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
+             "Tracing": [((tracing[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
+             "Standard": [((standard[i] / standard[i]) - 1) * 100 for i in range(len(standard))]})
+        # plot data
+        fig = sns.scatterplot(x='Run', y='value', hue='variable', data=pd.melt(data_preproc, ['Run']))
+        fig.set(xlabel='Run Number', ylabel='Percent Change from Standard')
+        fig.get_figure().savefig(f"Figures/figure_{run_num}.png")
+        # close the plot
+        plt.close()
+
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) > 1:
+        run_num = args[1]
+    else:
+        run_num = "5"
+    for _ in range(2, 6):
+        main(_)
+    main("C1")
