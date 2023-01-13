8d2190c0eb7fce08001cd8d2ce82622ba3f449c3
Jawand <jawandsingh@gmail.com>
2023-01-13 10:04:44 -0500

    fix visualizer.py

--- Tracing_experiment/visualizer.py
+++ Tracing_experiment/visualizer.py
@@ -2,13 +2,12 @@ import sys
 
 import pandas as pd
 import seaborn as sns
-import matplotlib.pyplot as plt
 
 args = sys.argv
 if len(args) > 1:
-    run_num = int(args[1])
+    run_num = args[1]
 else:
-    run_num = 4
+    run_num = "C1"
 with open(f"Results/result_{run_num}.txt") as file:
     #  get data
     lines = [line.rstrip() for line in file]
