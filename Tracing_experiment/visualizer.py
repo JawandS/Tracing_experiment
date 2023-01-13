06b98d5ba24d71f501e118bd7021002c7e10ea6e
Jawand <jawandsingh@gmail.com>
2023-01-13 09:14:29 -0500

    update processing

---
+++ Tracing_experiment/visualizer.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+run_num = 2
+with open(f"Results/result_{run_num}.txt") as file:
+    #  get data
+    lines = [line.rstrip() for line in file]
+    TIME = 20  # length of each job set
+    twoProbes = [TIME / elem for elem in eval(lines[8])]
+    tracing = [TIME / elem for elem in eval(lines[10])]
+    standard = [TIME / elem for elem in eval(lines[12])]
+    runNums = [i + 1 for i in range(len(twoProbes))]
+    data_preproc = pd.DataFrame(
+        {"Run": runNums, "Two Probes": [((twoProbes[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
+         "Tracing": [((tracing[i] / standard[i]) - 1) * 100 for i in range(len(standard))],
+         "Standard": [((standard[i] / standard[i]) - 1) * 100 for i in range(len(standard))]})
+    # plot data
+    fig = sns.scatterplot(x='Run', y='value', hue='variable', data=pd.melt(data_preproc, ['Run']))
+    fig.set(xlabel='Run Number', ylabel='Change from Standard (%)')
+    plt.show()
+    plt.savefig(f"Figures/figure_{run_num}.png")
