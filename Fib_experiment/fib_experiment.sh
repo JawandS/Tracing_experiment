22b0bc9f5661a3b1bc2414888ff8b4826a66e398
Jawand <jawandsingh@gmail.com>
2023-01-18 10:24:17 -0500

    update script

--- Fib_experiment/fib_experiment.sh
+++ Fib_experiment/fib_experiment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # setup
-git pull
-echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor # powersave or performance
+git pull -q
+sudo echo performance >>/sys/devices/system/cpu/cpu*/cpufreq/scaling_governor # powersave or performance
 increment=15
 threads=20
 depth=30
@@ -14,16 +14,18 @@ sudo bpftrace context_switch_probe.bt >>Logs/log_"$1".txt &
 # start workload
 counter=0
 end=$((SECONDS + increment))
-while [ $SECONDS -lt $end ]; do                                                   # continue for 10 seconds
-  python3 job.py $counter $threads $depth $isVariable >>/dev/null && counter=$((counter + 1)) # run job and increment counter
+while [ $SECONDS -lt $end ]; do
+  python3 job.py $counter $threads $depth "$isVariable" >>/dev/null && counter=$((counter + 1)) # run job and increment counter
 done
 # clear previous jobs
 killall -q python3
 killall -q bpftrace
 # process results
-echo "job: $1 | length: $increment | threads: $threads | depth: $depth | jobs: $counter | isVariable: $isVariable" | tee Results/results.txt # output to log
+result="job: $1 | length: $increment | threads: $threads | depth: $depth | jobs: $counter | isVariable: $isVariable"
+echo "$result"
+echo "$result" >>Results/results.txt # output to log
 #sudo python3 processing.py "$1"
 # add to git
-git add .
+git add -q .
 git commit -q -m "add and process $1"
-git push
+git push -q
