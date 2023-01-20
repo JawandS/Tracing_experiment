b9029d4beb7dcf4703d786cc757dccce9fb8a9c8
Jawand <jawandsingh@gmail.com>
2023-01-20 09:28:28 -0500

    update experiment

--- Tracing_experiment/exp.sh
+++ Tracing_experiment/exp.sh
@@ -12,15 +12,15 @@ experiment() {
   killall -q python3
   killall -q bpftrace
   truncate -s 0 raw.txt
-  sleep 1   # wait for 1 second
-  counter=0 # number of fib jobs completed
+  sleep 1 # wait for 1 second
   # run tracing if necessary
   if [ "$2" != "N" ]; then
     # shellcheck disable=SC2024
     sudo bpftrace Script/"$2".bt >>raw.txt & # being tracing
   fi
   # run the jobs and count how many get done
-  end=$((SECONDS + increment))    # 10 seconds
+  end=$((SECONDS + increment))
+  counter=0                       # number of fib jobs completed
   while [ $SECONDS -lt $end ]; do # continue for 10 seconds
     python3 job.py counter $threads $depth >>/dev/null &&
       counter=$((counter + 1)) # run job and increment counter
@@ -30,7 +30,7 @@ experiment() {
   # update logs
   echo $counter >>Logs/log_"$1".txt # add jobs done to log
   outputSize=$(wc -l raw.txt)
-  echo "$outputSize" >>Logs/log_"$1".txt               # add output size to log
+  echo "$outputSize" >>Logs/log_"$1".txt                    # add output size to log
   echo "Completed: $counter for $2 with $outputSize events" # output to console
 }
 # run experiment
@@ -38,11 +38,11 @@ iterationCounter=0
 for _ in {1..5}; do # number of iterations
   iterationCounter=$((iterationCounter + 1)) && printf "\t---------Run %s---------\n" "$iterationCounter"
   experiment "$1" N # base run
-  experiment "$1" A  # context switch
-  experiment "$1" B  # context switch + rcu
-  experiment "$1" C  # rcu
-  experiment "$1" D  # blank
-  experiment "$1" E  # rcu + enter sleep
+  experiment "$1" A # context switch
+  experiment "$1" B # context switch + rcu
+  experiment "$1" C # rcu
+  experiment "$1" D # blank
+  experiment "$1" E # rcu + enter sleep
 done
 python3 process.py "$1" $iterations $increment $threads $depth "$2" # run number, iterations, time, threads, depth, governor
 git add .
